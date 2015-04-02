25178e6e2173940f03f3d4dde4e8c92a78024a1a
rrajan <rrajan8@gatech.edu>
2015-04-01 21:46:12 -0400

    Total RX is broken

--- p3.cc
+++ p3.cc
@@ -30,6 +30,8 @@
 #include "ns3/olsr-helper.h"
 #include "ns3/basic-energy-source.h"
 #include "ns3/simple-device-energy-model.h"
+ #include "ns3/traced-value.h"
+#include "ns3/trace-source-accessor.h"
 
 #include <string>
  #include <math.h> 
@@ -38,38 +40,26 @@
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("WirelessAnimationExample");
+uint64_t count = 0;
 
-//NS_OBJECT_ENSURE_REGISTERED (OnOffApplication);
-
-// TypeId
-// OnOffApplication::GetTypeId (void)
-// {
-//   static TypeId tid = TypeId ("ns3::OnOffApplication")
-//     .SetParent<Application> ()
-//     .AddConstructor<OnOffApplication> ()
-//     .AddAttribute ("TotalTX",
-//                    "The maximum number of bytes sent by OnOffApplication.",
-//                    UintegerValue (0),
-//                    MakeUintegerAccessor (&OnOffApplication::m_totBytes),
-//                    MakeUintegerChecker<uint32_t> ())
-//     ;
-
-//   return tid;
-// }
-
-// uint32_t OnOffApplication::GetTotalTx()
-// {
-//   return TotalTX;
-// }
-
-OnOffApplication::OnOffApplication(){
-  m_totBytes = 0;
+void AddupBytes (const Ptr< const Packet > packet)
+{
+  count+=packet->GetSize();
 }
+
+
+
+
 
 int 
 main (int argc, char *argv[])
 {
+  RngSeedManager::SetSeed (11223344);
+  Ptr<UniformRandomVariable> U = CreateObject<UniformRandomVariable> ();
+  U->SetAttribute ("Stream", IntegerValue (6110));
+  U->SetAttribute ("Min", DoubleValue (0.0));
+  U->SetAttribute ("Max", DoubleValue (0.1));
+
   uint32_t nWifi = 50;
   double TxPower = 1.0;
   double trafficIntesity = 0.1;
@@ -89,6 +79,8 @@ main (int argc, char *argv[])
   
   Config::SetDefault ("ns3::OnOffApplication::DataRate", 
                       DataRateValue (DataRate (dataRate)));
+
+   Config::SetDefault("ns3::OnOffApplication::MaxBytes", UintegerValue(1024));
 
   NodeContainer allNodes;
   NodeContainer wifiStaNodes;
@@ -140,8 +132,23 @@ main (int argc, char *argv[])
     stack.SetRoutingHelper(olsr);
   }
 
+  uint32_t *senders = new uint32_t[nWifi];
 
-  
+  for (uint32_t i = 0; i < nWifi; ++i)
+    senders[i] = i;
+
+  for (uint32_t i = nWifi - 1; i > 0; --i) {
+    uint32_t j = rand() % i;
+    uint32_t tmp = senders[i];
+    senders[i] = senders[j];
+    senders[j] = tmp;
+  }
+
+  // for(uint32_t ii = 0; ii < nWifi; ii++)
+  // {
+  //   std::cout << ii << ":" << senders[ii] << std::endl;
+  // }
+
   stack.Install (allNodes);
 
   
@@ -151,28 +158,40 @@ main (int argc, char *argv[])
 
   Ipv4InterfaceContainer staInterfaces;
   staInterfaces = address.Assign (staDevices);
+  ApplicationContainer serverApps, clientApps;
+
+  for(uint32_t ii = 0; ii < nWifi; ii++)
+  {
+    PacketSinkHelper udpSink ("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(),9) );
+    serverApps.Add(udpSink.Install (wifiStaNodes.Get(senders[ii])));
 
 
-  PacketSinkHelper udpSink ("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(),9) );
-  ApplicationContainer serverApps = udpSink.Install (wifiStaNodes.Get (1));
+    OnOffHelper udpClient ("ns3::UdpSocketFactory", InetSocketAddress(staInterfaces.GetAddress(senders[ii]),9));
+    udpClient.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0.3]"));
+    udpClient.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=0.3]"));
+    clientApps.Add(udpClient.Install (wifiStaNodes.Get(ii)));
+    (clientApps.Get(ii))->SetStartTime(Seconds (U->GetValue()+1.0));
+  }
+
+  
   serverApps.Start (Seconds (1.0));
   serverApps.Stop (Seconds (15.0));
 
-  OnOffHelper udpClient ("ns3::UdpSocketFactory", InetSocketAddress(staInterfaces.GetAddress(1),9));
-
   
-
-  udpClient.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=0.3]"));
-  udpClient.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=0.3]"));
 
 
   //echoClient.SetAttribute ("MaxPackets", UintegerValue (10));
   //echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.)));
  // echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+
+  for(uint32_t ii = 0; ii < nWifi; ii++)
+  {
+    Ptr<OnOffApplication> source1 = DynamicCast<OnOffApplication> (clientApps.Get (ii));
+    source1->TraceConnectWithoutContext("Tx",MakeCallback(&AddupBytes));
+  }
   
-  ApplicationContainer clientApps = udpClient.Install (wifiStaNodes);
   
-  clientApps.Start (Seconds (1.0));
   clientApps.Stop (Seconds (15.0));
 
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
@@ -185,19 +204,35 @@ main (int argc, char *argv[])
       sprintf(buffer, "%d", i);
       anim.UpdateNodeDescription (wifiStaNodes.Get (i), buffer); // Optional
       anim.UpdateNodeColor (wifiStaNodes.Get (i), 255, 0, 0); // Optional
+      
     }
   
   anim.EnableIpv4RouteTracking ("routingtable-wireless.xml", Seconds (0), Seconds (5), Seconds (0.25)); //Optional
   anim.EnableWifiMacCounters (Seconds (0), Seconds (10)); //Optional
   anim.EnableWifiPhyCounters (Seconds (0), Seconds (10)); //Optional
+  anim.EnablePacketMetadata (); 
+
   Simulator::Run ();
   Simulator::Destroy ();
 
+  std::cout << "count: " << count << std::endl;
   //uint32_t bytes_sent;
-  for (uint32_t ii = 0; ii < nWifi; ii++)
-  {
-    Ptr<OnOffApplication> source1 = DynamicCast<OnOffApplication> (clientApps.Get (ii));
+  //for (uint32_t ii = 0; ii < nWifi; ii++)
+  //{
+    //Ptr<OnOffApplication> source1 = DynamicCast<OnOffApplication> (clientApps.Get (ii));
     //bytes_sent += source1->TotalTX;
+  //}
+  uint64_t sumRx = 0;
+  for(uint32_t ii = 0; ii < nWifi; ii++)
+  {
+    Ptr<PacketSink> sink1 = DynamicCast<PacketSink> (serverApps.Get (ii));
+    std::cout << ii << ":" << sink1->GetTotalRx() << std::endl;
+    sumRx+=sink1->GetTotalRx();
   }
+
+  std::cout << "RX:" << sumRx << std::endl;
+  std::cout << (double)nWifi/(1000.0*1000.0) << "," << TxPower << "," 
+  << route_protocol << "," << trafficIntesity  << "," << (double)sumRx/(double)count 
+  << std::endl;
   return 0;
 }
